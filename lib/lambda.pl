a5c1025056da1cfa41993ae0a9b6a3ca6baf166f
Leonid Mokrushin <likelion@gmail.com>
2021-11-30 12:14:33 +0100

    translated to DCG notation

--- lib/lambda.pl
+++ lib/lambda.pl
@@ -44,7 +44,7 @@ user:term_expansion(H === B, [H2 :- B2|L]) :-
   define(Mo:F/A),
   append(A0, [R], A1),
   H2 =.. [F|A1],
-  b_expand(B, B2, L, R),
+  phrase(b_expand(B, B2, R), L),
   succ(A, A2),
   export(Mo:F/A2).
 
@@ -53,71 +53,76 @@ define(T) :-
 define(T) :-
   assertz(fun(T)).
 
-b_expand(B, !, [], B) :-
-  var(B), !.
-b_expand(X if Y, B, L, R) :- !,
-  c_expand(Y, Y1, [], L0),
-  conj(Y1, !, Y2),
-  f_expand(X, Y2, B, L0, L, R).
-b_expand(X where Y, B, L, R) :- !,
-  c_expand(Y, Y1, [], L0),
-  f_expand(X, Y1, B, L0, L, R).
-b_expand(B0, B, L, R) :-
-  f_expand(B0, !, B, [], L, R).
+b_expand(B, !, B) -->
+  { var(B) }, !.
+b_expand(X if Y, B, R) --> !,
+  c_expand(Y, Y1),
+  { conj(Y1, !, Y2) },
+  f_expand(X, Y2, B, R).
+b_expand(X where Y, B, R) --> !,
+  c_expand(Y, Y1),
+  f_expand(X, Y1, B, R).
+b_expand(B0, B, R) -->
+  f_expand(B0, !, B, R).
 
 conj(A, true, A) :- !.
 conj(true, B, B) :- !.
 conj(A, B, (A,B)).
 
-c_expand(X, X, L, L) :-
-  var(X), !.
-c_expand((X,Y), B, L0, L) :- !,
-  c_expand(X, B1, L0, L1),
-  c_expand(Y, B2, L1, L),
-  conj(B1, B2, B).
-c_expand(X=Y, B, L0, L) :- !,
-  f_expand(X, true, B1, L0, L1, RX),
-  f_expand(Y, B1, B2, L1, L, RY),
-  conj(B2, (RX=RY), B).
-c_expand(X, X, L, L).
+c_expand(X, X) -->
+  { var(X) }, !.
+c_expand((X,Y), B) --> !,
+  c_expand(X, B1),
+  c_expand(Y, B2),
+  { conj(B1, B2, B) }.
+c_expand(X=Y, B) --> !,
+  f_expand(X, true, B1, RX),
+  f_expand(Y, B1, B2, RY),
+  { conj(B2, (RX=RY), B) }.
+c_expand(X, X) --> !.
 
-f_expand(X, Y, Y, L, L, X) :-
-  var(X), !.
-f_expand([A|B], Y0, Y, L0, L, [RA|RB]) :- !,
-  f_expand(A, Y0, Y1, L0, L1, RA),
-  f_expand(B, Y1, Y, L1, L, RB).
-f_expand(X, Y0, Y, L0, L, R) :-
-  callable(X),
-  strip_module(X, M, P),
-  P =.. [F|A0],
-  ( F == f
-  -> f_decomp(A0, Body, [], Bound, Free),
-     gensym('_f', Fun),
-     R =.. [Fun|Free],
-     Y = Y0,
-     append(Free, Bound, Args0),
-     append(Args0, [R1], Args),
-     Def =.. [Fun|Args],
-     f_expand(Body, true, Body2, L0, L1, R1),
-     L = [Def:-Body2|L1],
-     ( M == lambda
-     -> prolog_load_context(module, Mo)
-     ; Mo = M
-     ),
-     length(Args0, Ar),
-     define(Mo:Fun/Ar)
-  ; ( M == lambda
-    -> true
-    ; Mo = M
-    ),
-    length(A0, A),
-    fun(Mo:F/A), !,
-    append(A0, [R], A1),
-    a_expand(A1, A2, Y0, Y1, L0, L, R),
-    YY =.. [F|A2],
-    conj(Y1, YY, Y)
+f_expand(X, Y, Y, X) -->
+  { var(X) }, !.
+f_expand([A|B], Y0, Y, [RA|RB]) --> !,
+  f_expand(A, Y0, Y1, RA),
+  f_expand(B, Y1, Y, RB).
+f_expand(X, Y0, Y, R) -->
+  { callable(X),
+    strip_module(X, M, P),
+    P =.. [F|A0]
+  },
+  ( { F == f }
+  -> { f_decomp(A0, Body, [], Bound, Free),
+       gensym('_f', Fun),
+       R =.. [Fun|Free],
+       Y = Y0,
+       append(Free, Bound, Args0),
+       append(Args0, [R1], Args),
+       Def =.. [Fun|Args]
+     },
+     f_expand(Body, true, Body2, R1),
+     [Def:-Body2],
+     { ( M == lambda
+       -> prolog_load_context(module, Mo)
+       ; Mo = M
+       ),
+       length(Args0, Ar),
+       define(Mo:Fun/Ar)
+     }
+  ; { ( M == lambda
+      -> true
+      ; Mo = M
+      ),
+      length(A0, A),
+      fun(Mo:F/A), !,
+      append(A0, [R], A1)
+    },
+    a_expand(A1, A2, Y0, Y1, R),
+    { YY =.. [F|A2],
+      conj(Y1, YY, Y)
+    }
   ), !.
-f_expand(X, Y, Y, L, L, X).
+f_expand(X, Y, Y, X) --> !.
 
 f_decomp([Ta], Ta, Bo0, Bo, Fr) :- !,
   term_variables(Ta, F0),
@@ -133,7 +138,7 @@ without(S, [H|T], R) :-
   exclude(==(H),S,S1),
   without(S1, T, R).
 
-a_expand([R], [R], G, G, L, L, R) :- !.
-a_expand([H|T], [H2|T2], G0, G, L0, L, R) :-
-  f_expand(H, G0, G1, L0, L1, H2),
-  a_expand(T, T2, G1, G, L1, L, R).
+a_expand([R], [R], G, G, R) --> !.
+a_expand([H|T], [H2|T2], G0, G, R) -->
+  f_expand(H, G0, G1, H2),
+  a_expand(T, T2, G1, G, R).
